0251cd84355c5359b964a14daa371021645128c6
1Kuso4ek1 <dimasalov2006@gmail.com>
2024-12-04 22:40:47 +0300

    ...

--- app/src/main/java/com/example/practice/FormUiState.kt
+++ app/src/main/java/com/example/practice/FormUiState.kt
@@ -15,7 +15,7 @@ data class FormUiState(
                             .addConverterFactory(GsonConverterFactory.create())
                             .build(),
 
-    var selectedDate: LocalDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 20)),
+    var selectedDate: LocalDateTime = LocalDateTime.now(),
     val api: Schedule? = retrofit.create(Schedule::class.java),
     var response: ScheduleResponse? = null,
 
